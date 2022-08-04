4c55dd7e7aea6e8fdea4f755a6c0da9a6f894e6e
Rondolph S Martin <ronnie@Rondolphs-MacBook-Air.local>
2022-08-04 14:09:05 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
+import { useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // let count = 0;
+  const [count, setCount]= useState(0)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>My First React App</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You clicked the button {count} times.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={()=>{
+          console.log('i clicked the button'+ count);
+          // count =count +1;
+          setCount(count+1)
+        }}>add one</button>
       </header>
     </div>
   );
